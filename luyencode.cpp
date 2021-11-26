655300e59f5c68792efa9f6d13165fd968f92fc3
Lê Trọng Tùng <84660887+Tungyeudoi@users.noreply.github.com>
2021-11-26 23:33:24 +0700

    Add files via upload

---
+++ luyencode.cpp
@@ -0,0 +1,34 @@
+//-----------LeTrongTung-------------
+//-----------<3-KKL------------------
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define ull unsigned long long
+#define task1 "luyencode"
+#define task ""
+
+using namespace std;
+
+const int INF = 1e9+1;
+const int MaxN = 2e5+10;
+const ll oo = 1e18;
+
+void solve(){
+    ;
+}
+int main()
+{
+    srand(time(NULL));
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    if(fopen(task".inp", "r"))
+    {
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
+    }
+    int q;
+    cin >> q;
+    while(q--)
+        solve();
+}

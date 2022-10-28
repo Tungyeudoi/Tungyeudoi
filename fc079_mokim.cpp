22a3f08c2e51801737170b82fd978db15533265d
Lê Trọng Tùng <84660887+Tungyeudoi@users.noreply.github.com>
2022-10-28 22:15:45 +0700

    Create fc079_mokim.cpp

---
+++ fc079_mokim.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "480364801307831200 77028767876778803"; 
+    return 0;
+}
